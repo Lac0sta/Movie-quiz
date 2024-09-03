a57e5905143eae9d3ebc8c3a694e835a92dac391
Alekse1Frolov <aleksei.frolov@customertimes.com>
2024-09-04 01:07:35 +0200

    Extension with static colors added

--- Movie quiz/Helpers/UIColor+Extensions.swift
+++ Movie quiz/Helpers/UIColor+Extensions.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 extension UIColor {
-    
+    static var ypBlack: UIColor { UIColor(named: "YP Black iOS") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "YP White iOS") ?? UIColor.white }
+    static var ypGreen: UIColor { UIColor(named: "YP Green iOS") ?? UIColor.green }
+    static var ypRed: UIColor { UIColor(named: "YP Red iOS") ?? UIColor.red }
+    static var ypGray: UIColor { UIColor(named: "YP Gray iOS") ?? UIColor.gray }
+    static var ypDarkGray: UIColor { UIColor(named: "YP Dark gray iOS") ?? UIColor.darkGray }
 }

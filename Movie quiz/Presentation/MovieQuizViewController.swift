72955602868c0329a57f599e38479cf71f9726e9
Alekse1Frolov <aleksei.frolov@customertimes.com>
2024-09-24 20:53:42 +0200

    Implemented: question display, answer validation, and quiz result display

--- Movie quiz/Presentation/MovieQuizViewController.swift
+++ Movie quiz/Presentation/MovieQuizViewController.swift
@@ -26,48 +26,58 @@ struct QuizStepViewModel {
     let questionNumber: String
 }
 
+// View model for the "Quiz Result" state
+struct QuizResultViewModel {
+    // The alert title
+    let title: String
+    // The text about the number of points scored
+    let text: String
+    // The text for the alert button
+    let butonText: String
+}
+
 final class MovieQuizViewController: UIViewController {
     
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             movieImage: "The Godfather",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 9,2
             isCorrectAnswer: true),
         QuizQuestion(
             movieImage: "The Dark Knight",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 9
             isCorrectAnswer: true),
         QuizQuestion(
             movieImage: "Kill Bill",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 8,1
             isCorrectAnswer: true),
         QuizQuestion(
             movieImage: "The Avengers",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 8
             isCorrectAnswer: true),
         QuizQuestion(
             movieImage: "Deadpool",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 8
             isCorrectAnswer: true),
         QuizQuestion(
             movieImage: "The Green Knight",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 6,6
             isCorrectAnswer: true),
         QuizQuestion(
             movieImage: "Old",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 5,8
             isCorrectAnswer: false),
         QuizQuestion(
             movieImage: "The Ice Age Adventures of Buck Wild",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 4.3
             isCorrectAnswer: false),
         QuizQuestion(
             movieImage: "Tesla",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 5,1
             isCorrectAnswer: false),
         QuizQuestion(
             movieImage: "Vivarium",
-            questionText: "Is the rating of this movie more than 6?",
+            questionText: "Is the rating of this movie more than 6?", // Rating: 5,8
             isCorrectAnswer: false)
     ]
     private var currentQuestionIndex = 0
@@ -85,11 +95,13 @@ final class MovieQuizViewController: UIViewController {
     }
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
-        
+        let givenAnswer = true
+        showAnswerResult(isCorrect: questions[currentQuestionIndex].isCorrectAnswer == givenAnswer)
     }
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
-        
+        let givenAnswer = false
+        showAnswerResult(isCorrect: questions[currentQuestionIndex].isCorrectAnswer == givenAnswer)
     }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
@@ -106,68 +118,47 @@ final class MovieQuizViewController: UIViewController {
         textLabel.text = step.question
     }
     
+    private func showAnswerResult(isCorrect: Bool) {
+        
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.cornerRadius = 20
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResult()
+        }
+    }
     
+    private func showNextQuestionOrResult() {
+        if currentQuestionIndex == questions.count - 1 {
+            
+            showAlertWith(result: QuizResultViewModel(title: "This round is finished",
+                                                      text: "Your score is \(correctAnswers)",
+                                                      butonText: "Play again"))
+            
+        } else {
+            currentQuestionIndex += 1
+            showQuestion(quiz: convert(model: questions[currentQuestionIndex]))
+        }
+    }
+    
+    private func showAlertWith(result model: QuizResultViewModel) {
+        let alert = UIAlertController(title: model.title,
+                                      message: model.text,
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: model.butonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            self.showQuestion(quiz: self.convert(model: self.questions[self.currentQuestionIndex]))
+        }
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
-/*
- Mock-data
- 
- Picture: The Godfather
- Rating: 9,2
- Question: Is the rating of this movie more than 6?
- Answer: YES
- 
- 
- Picture: The Dark Knight
- Rating: 9
- Question: Is the rating of this movie more than 6?
- Answer: YES
- 
- 
- Picture: Kill Bill
- Rating: 8,1
- Question: Is the rating of this movie more than 6?
- Answer: YES
- 
- 
- Picture: The Avengers
- Rating: 8
- Question: Is the rating of this movie more than 6?
- Answer: YES
- 
- 
- Picture: Deadpool
- Rating: 8
- Question: Is the rating of this movie more than 6?
- Answer: YES
- 
- 
- Picture: The Green Knight
- Rating: 6,6
- Question: Is the rating of this movie more than 6?
- Answer: YES
- 
- 
- Picture: Old
- Rating: 5,8
- Question: Is the rating of this movie more than 6?
- Answer: NO
- 
- 
- Picture: The Ice Age Adventures of Buck Wild
- Rating: 4,3
- Question: Is the rating of this movie more than 6?
- Answer: NO
- 
- 
- Picture: Tesla
- Rating: 5,1
- Question: Is the rating of this movie more than 6?
- Answer: NO
- 
- 
- Picture: Vivarium
- Rating: 5,8
- Question: Is the rating of this movie more than 6?
- Answer: NO
- */
